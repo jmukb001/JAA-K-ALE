3d887b6c02a213bcbcde57b79370069b6cf80112
Elena <elenamardi03@gmail.com>
2023-04-29 14:02:06 -0700

    ready to start

---
+++ pet.py
@@ -0,0 +1,18 @@
+# class Pet: 
+#   def __init__(self, name, mood):
+#     self.name = name
+#     self.mood = mood
+
+# #making a pet 
+# p1 = Pet("Lucy", "happy")
+
+# print(p1.name)
+# print(p1.age)
+
+def add_numbers():
+    x = int(78)
+    y = int(32)
+    result = x + y
+    return result
+
+print(add_numbers())
