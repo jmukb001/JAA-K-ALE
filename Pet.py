96d4ed6d05e1a184b5c5fd08462385833a158834
arrdyas <arrdyas@gmail.com>
2023-04-29 21:43:15 -0700

    Added Pet.py

---
+++ Pet.py
@@ -0,0 +1,69 @@
+class Pet:
+    happy = 50
+    energy = 50
+    food = 50
+    sleeping = True
+    
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+    
+    def decHappy(self, decBy):
+        if self.happy > 0 & self.happy - decBy >= 0:
+            self.happy -= decBy
+        elif selfhappy > 0:
+            self.happy = 0
+    
+    def decEnergy(self, decBy):
+        if self.energy > 0 & self.energy - decBy >= 0:
+            self.energy -= decBy
+        elif self.energy > 0:
+            self.nergy = 0
+    
+    def decFood(self, decBy):
+        if self.food > 0 & self.food - decBy >= 0:
+            self.food -= decBy
+        elif self.food > 0:
+            self.food = 0
+    
+    def incHappy(self, incBy):
+        if self.happy < 50 & self.happy + incBy <= 50:
+            self.happy += incBy
+        elif self.happy < 50:
+            self.happy = 50
+    
+    def incEnergy(self, incBy):
+        if self.energy < 50 & self.energy + incBy <= 50:
+            self.energy += incBy
+        elif self.energy < 50:
+            self.energy = 50
+            
+    def incFood(self, incBy):
+        if self.food < 50 & self.food + incBy <= 50:
+            self.food += incBy
+        elif self.food < 50:
+            self.food = 50
+    
+    def feed(self, feedVal):
+        if self.sleeping:
+            print("Your pet is sleeping")
+        elif feedVal == 1:
+            self.incFood(5)
+        else:
+            self.decHappy(5)
+    
+    def putToSleep(self, sleepVal):
+        if self.sleeping:
+            print("Your pet is sleeping")
+        elif self.energy < 20 & sleepVal == 1:
+            self.sleeping = true;
+        elif sleepVal == 1:
+            self.decHappy(5)
+    
+    def play(self, winVal):
+        if self.sleeping:
+            print("Your pet is sleeping")
+        elif winVal < 1:
+            self.decHappy(5)
+        elif winVal > 0:
+            self.incHappy(5)
